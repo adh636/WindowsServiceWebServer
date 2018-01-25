a7e7b7b6fbb0e0fa17aa1cc6e4491e5e8894fdbb
Adam Hilliard <adh636@gmail.com>
2018-01-25 01:08:01 -0500

    Change directory name for serving up static content

--- ExpressWebServer.js
+++ ExpressWebServer.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var app = express();
 
-app.use(express.static(__dirname + "/dist"));
+app.use(express.static(__dirname + "/CineNetWebUI"));
 
 var server = app.listen(8080, function () {
    var host = server.address().address
